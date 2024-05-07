60b49fe99f6e34b6185bdf0767f69492ddf6d69d
Kaique Pires <manozulukid@gmail.com>
2024-05-07 14:11:03 -0300

    Exemplifying a class anatomy and immutable variables by name and reserved word convention

---
+++ sintaxe/src/edu/kaique/anatomiadeclase/MinhaClasse.java
@@ -0,0 +1,10 @@
+package edu.kaique.anatomiadeclase;
+
+public class MinhaClasse {
+    public static void main(String [] args){
+        System.out.println("Hello, world!");
+
+        // Variaveis que não alteram escrevemos em maiusculo e podemos utilizar a palavra reservada "final" para deixar a variavel imutavel
+        final String BR = "Brasil";
+    }
+}
