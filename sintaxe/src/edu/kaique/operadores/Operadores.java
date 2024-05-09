51df35f323608083995b67abc070856b543a15a9
Kaique Pires <manozulukid@gmail.com>
2024-05-08 22:33:00 -0300

    Operators

---
+++ sintaxe/src/edu/kaique/operadores/Operadores.java
@@ -0,0 +1,10 @@
+package edu.kaique.operadores;
+
+public class Operadores {
+
+    // Operador de atribuição "="
+    // Operadores aritmeticos + (adicao e concatenacao), - (subtracao), * (multiplicacao) e / (divisao)
+    // ++(incremento), -- (decremento), ! (negacao), condicao ? se satisfaz : se nao satisfaz (operador ternario)
+    // ==(igual a), != (diferente), > (maior que), >= (maior ou igual), < (menor que) e <= (menor ou igual)
+    // && (operador logico E) e || (operador logico OU)
+}
