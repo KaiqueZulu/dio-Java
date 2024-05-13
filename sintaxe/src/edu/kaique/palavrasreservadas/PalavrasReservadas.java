1f8d6fb13a80583e7425fbb915c58da170b65616
Kaique Pires <manozulukid@gmail.com>
2024-05-12 23:58:23 -0300

    Reserved words

---
+++ sintaxe/src/edu/kaique/palavrasreservadas/PalavrasReservadas.java
@@ -0,0 +1,81 @@
+package edu.kaique.palavrasreservadas;
+
+public class PalavrasReservadas {
+    /*
+        * Tipos primitivos
+        * Classificacoes
+        * Escopo de uso
+        * Palavras "opostas"
+
+     Java possui 52 palavras reservadas.
+    */
+
+    /*
+    Controles de pacotes
+        * import = importa pacotes ou classes
+        * package = define o pacote
+    */
+
+    /*
+    Modificadores de acesso
+        * public = qualquer um pode acessar
+        * private = somente dentro do pacote
+        * protected = dentro do pacote e subclasses(Heranca)
+    */
+
+    /*
+    Primitivos
+        * boolean
+        * byte = um inteiro de 8 bits (-128 a 127)
+        * char = um caractere unicode (16 bits)
+        * double = ponto flutuante (64 bits)
+        * float = ponto flutuante (32 bits)
+        * int = inteiro (32 bits)
+        * long = inteiro (64 bits)
+        * short = inteiro (16 bits)
+        * void = ausencia de um valor
+    */
+
+    /*
+    Modificadores de classes, variaveis e metodos
+        * abstract = classe que não pode ser instanciada ou metodos que precisa ser implementado por subclasses
+        * class = define uma classe
+        * extends = herdar uma classe
+        * final = impossibilita que uma classe seja estendida, que um metodo seja sobrescrito e uma variavel, seja reinicializada
+        * interface = define uma interface
+        * implements = implementar uma interface
+        * native = define um metodo nativo
+        * new = cria um novo objeto
+        * static = variavel ou metodo statico
+        * strictfp = define o ponto flutuante
+        * synchronized = sincronizado thread
+        *transient = impede a serializacao de campos
+        * volatile = indica que uma variavel pode ser alterada durante o uso de threads
+    */
+
+    /*
+    Controle de fluxo num bloco de codigo
+        * break = interrompe o fluxo de um laço
+        * case = define uma condicao para um switch
+        * continue = pula para o proximo laço
+    */
+
+    /*
+    Tratamento de erros
+        * assert = define uma condicao que deve ser verdadeira
+        * catch = declara o bloco de exceções
+        * finally = bloco dew codigo apos um try-catch
+        * throw = lanca uma exceção
+        * throws = indica que uma exceção pode ser lancada em um metodo
+        * try = define um bloco de exceções
+    */
+
+    /*
+    Variaveis de referencia
+        * null = variavel que contem o valor nulo
+        * this = variavel que contem o proprio objeto
+        * super = variavel que contem o objeto pai
+    */
+    
+
+}
