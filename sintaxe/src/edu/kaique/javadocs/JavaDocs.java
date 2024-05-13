f1ac54881a28ac8e7e3bfdee12d4c47351500738
Kaique Pires <manozulukid@gmail.com>
2024-05-13 00:56:28 -0300

    Java Docs

---
+++ sintaxe/src/edu/kaique/javadocs/JavaDocs.java
@@ -0,0 +1,15 @@
+package edu.kaique.javadocs;
+
+public class JavaDocs {
+    /**
+    *@author Kaique
+    * @version 1.0
+    * @since 2022
+    * @param args
+    * @return void
+    * @throws IOException
+    *  */
+    public static void main(String[] args) {
+
+    }
+}
